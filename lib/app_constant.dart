eada2d8fad680c15f5a901fc53956c374a2970de
humamafif <afifalmahbubi04@gmail.com>
2024-07-28 23:06:26 +0700

    initial setup project

---
+++ lib/app_constant.dart
@@ -0,0 +1,6 @@
+class AppConstant {
+  static const String baseURL = "https://api.themoviedb.org/3/";
+  static const String apikey = "c8f1b0d8b95fb5c7e7c29ddd22486d1a";
+  static const String imagew500 = "https://image.tmdb.org/t/p/w500";
+  static const String imageOriginal = "https://image.tmdb.org/t/p/original";
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+  FlutterNativeSplash.remove();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
